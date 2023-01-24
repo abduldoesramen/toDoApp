7ecca5f9448b156948e6f5a0b1ffacfa8dde004b
abdul <abdulrehman133782@gmail.com>
2023-01-24 15:10:14 +1100

    basic list working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
+import { TextField } from "@mui/material/";
 
 const App = () => {
-  const currList = [
+  const [value, setValue] = useState("");
+  const [events, setEvents] = useState([
     {
       id: "1",
       eventName: "Do the dishes",
@@ -18,18 +21,50 @@ const App = () => {
       id: "4",
       eventName: "Scrub the bathroom",
     },
-  ];
+  ]);
+
+  const handleChange = (eventData) => {
+    setValue(eventData.target.value);
+  };
+
+  const handleNewEvent = () => {
+    const updateEvents = [
+      {
+        id: "1",
+        eventName: "test",
+      },
+    ];
+    setEvents((events) => [events, updateEvents]);
+    console.log(`${events}`);
+  };
+
+  useEffect(() => {
+    setEvents(events);
+  }, [events]);
 
   return (
     <div className="list-container">
-      <div className="card-list-container">
-        <div className="card-container">
-          {currList.map(({ eventName }) => (
-            <h2>{eventName}</h2>
-          ))}
-        </div>
+      <div className="card-container">
+        {events.map(({ eventName, id }) => (
+          <h2>
+            {id}: {eventName}
+          </h2>
+        ))}
       </div>
-      <Button variant="contained">Add To List</Button>
+      <div>
+        <TextField
+          value={value}
+          id="outlined-basic"
+          label="Enter event"
+          variant="outlined"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <Button variant="contained" onClick={handleNewEvent}>
+          Add To List
+        </Button>
+      </div>
     </div>
   );
 };
