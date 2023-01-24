55e29938ba24402da252ff30af9a1e0002e42c78
abdul <abdulrehman133782@gmail.com>
2023-01-24 14:20:02 +1100

    added hard-coded list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import Button from "@mui/material/Button";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const currList = [
+    {
+      id: "1",
+      eventName: "Do the dishes",
+    },
+    {
+      id: "2",
+      eventName: "Take out the trash",
+    },
+    {
+      id: "3",
+      eventName: "Vacuum the floor",
+    },
+    {
+      id: "4",
+      eventName: "Scrub the bathroom",
+    },
+  ];
+
+  return (
+    <div className="list-container">
+      <div className="card-list-container">
+        <div className="card-container">
+          {currList.map(({ eventName }) => (
+            <h2>{eventName}</h2>
+          ))}
+        </div>
+      </div>
+      <Button variant="contained">Add To List</Button>
+    </div>
+  );
 };
 
 export default App;
