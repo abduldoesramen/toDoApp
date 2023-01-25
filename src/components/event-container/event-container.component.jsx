084ea7c862a91d96e46af8abe06c27a5f46d4b72
abdul <abdulrehman133782@gmail.com>
2023-01-25 16:45:38 +1100

    checks

--- src/components/event-container/event-container.component.jsx
+++ src/components/event-container/event-container.component.jsx
@@ -2,11 +2,11 @@ import "./event-container.styles.css";
 
 const EventContainer = ({ events }) => {
     return (
-        <div className="event-container" key={events.id}>
-            {events.map(({eventName, id}) => {
-                return <h2 key={id}>{`${id}. ${eventName}`}</h2>
-            })}
-        </div>
+            <div className="event-container" key={events.id}>
+                {events.map(({eventName, id}) => {
+                    return <h1 key={id}>{`${id}. ${eventName}`}</h1>
+                })}
+            </div>
     )
 }
 
