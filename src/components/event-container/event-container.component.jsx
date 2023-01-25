554d119e0789cc57cd8a1b5ba29a22903f3a8e37
abdul <abdulrehman133782@gmail.com>
2023-01-25 11:43:39 +1100

    fixed rendering, event-container is refactored

--- src/components/event-container/event-container.component.jsx
+++ src/components/event-container/event-container.component.jsx
@@ -1,18 +1,13 @@
 import "./event-container.styles.css";
 
 const EventContainer = ({ events }) => {
-    // return <h1>Hello World! {events}</h1>
-    
-
-    <div className="event-container" key={events.id}>
-        return (
-            {events.map(({ eventName }) => {
-                {console.log(`${eventName}`)}
-                    <h2>{eventName}</h2>
+    return (
+        <div className="event-container" key={events.id}>
+            {events.map(({eventName}) => {
+                return <h2>{eventName}</h2>
             })}
-        )
-
-    </div>
+        </div>
+    )
 }
 
 export default EventContainer;
