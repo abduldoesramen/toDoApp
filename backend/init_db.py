242c3aee535bce649f42452e8c303a1c9c920240
abdul <abdulrehman133782@gmail.com>
2023-02-02 11:21:46 +1100

    added very basic postgres databse with two tables: users and events

--- backend/init_db.py
+++ backend/init_db.py
@@ -3,7 +3,7 @@ import psycopg2
 
 conn = psycopg2.connect(
         host="localhost",
-        database="flask_db",
+        database="todoapp_db",
         user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
 
@@ -11,32 +11,45 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute('DROP TABLE IF EXISTS books;')
-cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                 'title varchar (150) NOT NULL,'
-                                 'author varchar (50) NOT NULL,'
-                                 'pages_num integer NOT NULL,'
-                                 'review text,'
-                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+cur.execute('DROP TABLE IF EXISTS users;')
+cur.execute('CREATE TABLE users (UserId serial PRIMARY KEY,'
+                                 'email varchar (50) NOT NULL,'
+                                 'password varchar (50) NOT NULL);'
+                                 )
+cur.execute('CREATE TABLE events (EventId serial NOT NULL PRIMARY KEY,'
+                                 'event varchar (300) NOT NULL,'
+                                 'uid integer REFERENCES users(UserId));'
                                  )
 
-# Insert data into the table
-
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('A Tale of Two Cities',
-             'Charles Dickens',
-             489,
-             'A great classic!')
+# Insert dummy data to test into the table
+cur.execute('INSERT INTO users (email, password)'
+            'VALUES (%s, %s)',
+            ('abdul@anemail.com',
+             'chickenwithrice')
             )
 
+cur.execute('INSERT INTO users (email, password)'
+            'VALUES (%s, %s)',
+            ('softwareEngenius@rice.com',
+             'iCodeAllDay')
+            )
 
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('Anna Karenina',
-             'Leo Tolstoy',
-             864,
-             'Another great classic!')
+# Insert dummy toDoList strings associated with the users above
+cur.execute('INSERT INTO events (event, uid)'
+            'VALUES (%s, %s)',
+            ('take out the rubbish',
+             1)
+            )
+cur.execute('INSERT INTO events (event, uid)'
+            'VALUES (%s, %s)',
+            ('clean the dishes',
+             1)
+            )
+
+cur.execute('INSERT INTO events (event, uid)'
+            'VALUES (%s, %s)',
+            ('program code',
+             2)
             )
 
 conn.commit()
