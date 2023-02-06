6b0fbabbfd1e682a97b74f641c50022a7c5d24e5
abdul <abdulrehman133782@gmail.com>
2023-02-06 15:48:11 +1100

    added basic database functionality

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 import shortuuid
+import time
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, Table
@@ -25,6 +26,18 @@ database=os.getenv('database')
 # app.app_context().push()
 # all_users = db.Table('users', db.metadata, autoload=True, autoload_with=db.engine)
 
+# Array to hold all previous values
+numbers = [1, 2, 3, 4, 5]
+
+# Function to return an ordered, unique integer
+ordered_number = 0
+def return_random(ordered_number):
+    if ordered_number not in numbers:
+        return ordered_number
+    else:
+        ordered_number += 1
+        return_random(ordered_number)
+
 @app.route('/')
 def index():
     # results = db.session.query(all_users).all()
@@ -44,8 +57,8 @@ def index():
 def add_user(email, userpassword): 
     conn = psycopg2.connect(f"postgresql://{username}:{password}@localhost:5432/{database}")
     cur = conn.cursor()
-    cur.execute('INSERT INTO users (email, password) VALUES (%s, %s, %s)',
-        (shortuuid.uuid(name="email/password"), email, userpassword)
+    cur.execute('INSERT INTO users (UserId, email, password) VALUES (%s, %s, %s)',
+        (shortuuid.uuid(), email, userpassword)
     )
     conn.commit()
     cur.close()
--- backend/init_db.py
+++ backend/init_db.py
@@ -1,56 +1,76 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
+import shortuuid
+
+load_dotenv()
+
+# Env private attributes
+username=os.getenv('username')
+password=os.getenv('password')
+database=os.getenv('database')
 
 conn = psycopg2.connect(
         host="localhost",
         database="todoapp_db",
-        user=os.environ['username'],
-        password=os.environ['password'])
+        user=username,
+        password=password)
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
+userone = shortuuid.uuid()
+usertwo = shortuuid.uuid()
+
+
 # Execute a command: this creates a new table
 cur.execute('DROP TABLE IF EXISTS events;')
 cur.execute('DROP TABLE IF EXISTS users;')
-cur.execute('CREATE TABLE users (UserId serial PRIMARY KEY,'
+cur.execute('CREATE TABLE users (UserId varchar (100) PRIMARY KEY,'
                                  'email varchar (50) NOT NULL,'
                                  'password varchar (50) NOT NULL);'
                                  )
-cur.execute('CREATE TABLE events (EventId serial NOT NULL PRIMARY KEY,'
+cur.execute('CREATE TABLE events (EventId varchar (100) NOT NULL PRIMARY KEY,'
                                  'event varchar (300) NOT NULL,'
-                                 'uid integer REFERENCES users(UserId));'
+                                 'uid varchar REFERENCES users(UserId));'
                                  )
 
 # Insert dummy data to test into the table
-cur.execute('INSERT INTO users (email, password)'
-            'VALUES (%s, %s)',
-            ('abdul@anemail.com',
+cur.execute('INSERT INTO users (UserId, email, password)'
+            'VALUES (%s, %s, %s)',
+            (
+             userone,
+             'abdul@anemail.com',
              'chickenwithrice')
             )
 
-cur.execute('INSERT INTO users (email, password)'
-            'VALUES (%s, %s)',
-            ('softwareEngenius@rice.com',
+cur.execute('INSERT INTO users (UserId, email, password)'
+            'VALUES (%s, %s, %s)',
+            (
+             usertwo,
+             'softwareEngenius@rice.com',
              'iCodeAllDay')
             )
 
 # Insert dummy toDoList strings associated with the users above
-cur.execute('INSERT INTO events (event, uid)'
-            'VALUES (%s, %s)',
-            ('take out the rubbish',
-             1)
+cur.execute('INSERT INTO events (EventId, event, uid)'
+            'VALUES (%s, %s, %s)',
+            (shortuuid.uuid(),
+             'take out the rubbish',
+             userone)
             )
-cur.execute('INSERT INTO events (event, uid)'
-            'VALUES (%s, %s)',
-            ('clean the dishes',
-             1)
+cur.execute('INSERT INTO events (EventId, event, uid)'
+            'VALUES (%s, %s, %s)',
+            (shortuuid.uuid(),
+             'clean the dishes',
+             userone)
             )
 
-cur.execute('INSERT INTO events (event, uid)'
-            'VALUES (%s, %s)',
-            ('program code',
-             2)
+cur.execute('INSERT INTO events (EventId, event, uid)'
+            'VALUES (%s, %s, %s)',
+            (shortuuid.uuid(),
+             'program code',
+             usertwo)
             )
 
 conn.commit()
