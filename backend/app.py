0e3c0a65385ebbce219c7bb96bb9a8e38b9341da
abdul <abdulrehman133782@gmail.com>
2023-02-07 12:32:29 +1100

    harperDB functionality successfully working with Flask but not connected to postgres yet

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,9 @@ import os
 from dotenv import load_dotenv
 import shortuuid
 import time
+import harperdb
 
 load_dotenv()
-
-app = Flask(__name__)
 
 # PostgreSQL Database credentials loaded from the .env file
 username=os.getenv('username')
@@ -16,6 +15,17 @@ password=os.getenv('password')
 database=os.getenv('database')
 
 # HarperDB Database credentials loaded from the .env file
+harperdb_url = os.getenv('harperdb_url')
+harperdb_username = os.getenv('harperdb_username')
+harperdb_password = os.getenv('harperdb_password')
+
+app = Flask(__name__)
+
+db = harperdb.HarperDB(
+    url=harperdb_url,
+    username=harperdb_username,
+    password=harperdb_password
+)
 
 CORS(app)
 
@@ -28,12 +38,33 @@ def add_new_user():
     if request.method == 'POST':
         data = request.args.to_dict()
         print(data)
-        # emailValue = request.args.get('emailValue')
-        # passwordValue = request.args.get('passwordValue')
         cur.execute("INSERT INTO users (UserId, email, password) VALUES (%s, %s, %s)",
         (f"{shortuuid.uuid()}", f"{data['emailValue']}", f"{data['passwordValue']}")
         )
         conn.commit()
+        return 'Form submitted'
+    else:
+        return 'Form submission failed'
+
+# HarperDB test routes
+
+@app.route('/harperdb')
+def harperdb_fetch_all():
+    fetch_all = db._sql('SELECT * FROM dev.users')
+    print(fetch_all)
+    return jsonify(fetch_all)
+
+@app.route('/harperdb/add-user', methods=['GET', 'POST'])
+def harperdb_add_user():
+    if request.method == 'POST':
+        data = request.args.to_dict()
+        print(data)
+
+        (f"{shortuuid.uuid()}", f"{data['email']}", f"{data['password']}")
+        add_new_user = db._sql(
+            f"INSERT INTO dev.users(UserId, email, password) VALUES ('{shortuuid.uuid()}', '{data['email']}', '{data['password']}')" 
+        )
+        print(add_new_user)
         return 'Form submitted'
     else:
         return 'Form submission failed'
@@ -79,14 +110,14 @@ try:
         return f'User with Email: {email} has been deleted.'
 
     # Add user via URL with username and password (CHANGE LATER)
-    @app.route('/<email>/<userpassword>', methods=['GET', 'POST'])
-    def add_user(email, userpassword): 
-        #if request.method == 'POST':
-        cur.execute('INSERT INTO users (UserId, email, password) VALUES (%s, %s, %s)',
-            (shortuuid.uuid(), email, userpassword)
-        )
-        conn.commit()
-        return 'ran'
+    # @app.route('/<email>/<userpassword>', methods=['GET', 'POST'])
+    # def add_user(email, userpassword): 
+    #     #if request.method == 'POST':
+    #     cur.execute('INSERT INTO users (UserId, email, password) VALUES (%s, %s, %s)',
+    #         (shortuuid.uuid(), email, userpassword)
+    #     )
+    #     conn.commit()
+    #     return 'ran'
 
     # Time, for displaying on Front-end
     @app.route('/time')
